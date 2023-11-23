c6cd1bee24153026fcbe3115196aacc665e08e5c
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-11-23 03:47:13 +0100

    menu dropdown

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var cancelBtn = document.querySelector("#cancel");
 var notice = document.querySelector("#notice")
+var svg = document.querySelector("#svg")
+var username = document.querySelector("#username")
+var menu = document.querySelector("#dropdown")
 
 cancelBtn.addEventListener('click', () => {
     notice.style.display = "none"
 })
+
+username.addEventListener('click', (event) => {
+    event.stopPropagation();
+    menu.classList.toggle('visible')
+})
+
+document.body.addEventListener('click', function () {
+    menu.classList.add('visible');
+});
+
+menu.addEventListener('click', function (event) {
+    event.stopPropagation();
+});
