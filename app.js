0bb25b6115ac60e1b94d20aa4c8b32602975113b
Lkjhhgffdssaaa <Stephenon56@gmail.com>
2023-11-23 04:15:32 +0100

    alert. bug dey sha

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 var cancelBtn = document.querySelector("#cancel");
-var notice = document.querySelector("#notice")
-var svg = document.querySelector("#svg")
-var username = document.querySelector("#username")
-var menu = document.querySelector("#dropdown")
+var notice = document.querySelector("#notice");
+var svg = document.querySelector("#svg");
+var username = document.querySelector("#username");
+var menu = document.querySelector("#dropdown");
+var notification = document.querySelector('#alert');
 
 cancelBtn.addEventListener('click', () => {
-    notice.style.display = "none"
-})
+    notice.style.display = "none";
+});
 
-username.addEventListener('click', (event) => {
+// Notification menu
+svg.addEventListener('click', (event) => {
     event.stopPropagation();
-    menu.classList.toggle('visible')
-})
+    notification.classList.toggle('hidden');
+});
 
 document.body.addEventListener('click', function () {
-    menu.classList.add('visible');
+    notification.classList.add('hidden');
+});
+
+notification.addEventListener('click', function (event) {
+    event.stopPropagation();
+});
+
+// Dropdown menu 
+username.addEventListener('click', (event) => {
+    event.stopPropagation();
+    menu.classList.toggle('visible');
+});
+
+document.body.addEventListener('click', function () {
+    menu.classList.remove('visible');
 });
 
 menu.addEventListener('click', function (event) {
